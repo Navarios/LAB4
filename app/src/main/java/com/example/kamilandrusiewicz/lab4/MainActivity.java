41654a92a27c1c4526fce7abf71f82fa4692263c
Kamil Andrusiewicz <navarios21@gmail.com>
2018-04-23 21:07:56 +0200

    LAB4 zadanie 1

--- app/src/main/java/com/example/kamilandrusiewicz/lab4/MainActivity.java
+++ app/src/main/java/com/example/kamilandrusiewicz/lab4/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.kamilandrusiewicz.lab4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO sprawdzenie
+        String[] values=new String[]{"Pies","Kot","Koń","Gołąb","Kruk"};
+        this.target=new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+
+        ListView listView=(ListView)findViewById(R.id.lista);
+        listView.setAdapter(this.adapter);
+
     }
 }
