94569a80d469ed55dd020cbecaa5af4d35047a09
Kamil Andrusiewicz <navarios21@gmail.com>
2018-04-24 10:52:06 +0200

    LAB4 zadanie 2

--- app/src/main/java/com/example/kamilandrusiewicz/lab4/MainActivity.java
+++ app/src/main/java/com/example/kamilandrusiewicz/lab4/MainActivity.java
@@ -34,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater=getMenuInflater();
-        inflater.inflate()(R.menu.main_menu,menu);
+        inflater.inflate(R.menu.main_menu,menu);
         return true;
     }
 
     public void nowyWpis(MenuItem mi){
         Intent intencja=new Intent(this,DodajWpis.class);
         startActivityForResult(intencja,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1&& resultCode==RESULT_OK){
+            Bundle extras=data.getExtras();
+            String nowy=(String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
